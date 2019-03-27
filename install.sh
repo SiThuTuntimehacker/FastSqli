bc7eae21d36a54d20b0301181c9b5b6354d025b3
SiThuTun <40698550+SiThuTuntimehacker@users.noreply.github.com>
2019-03-27 13:40:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt install python2
 apt install python3
 apt install python
 git clone https://github.com/sqlmapproject/sqlmap
+cp -r sqlmap $HOME
 apt install figlet 
 apt install toilet
 pkg install figlet
